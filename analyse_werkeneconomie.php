e3376d8b31d1e7405b72fac578e85158756c1aaa
ianrau9 <albireo9876@gmail.com>
2018-11-09 17:28:17 +0100

    Add files via upload
    eerste versie werk index analyse

---
+++ analyse_werkeneconomie.php
@@ -0,0 +1,29 @@
+<?
+class werkIndex
+{
+	public:
+	//opvragen van de werkIndex array
+	function get()
+	{
+		return generateWerk();
+	}
+	
+	private:
+	
+	function generateWerk()
+	{
+		$waarden = array('gentpoort');
+		$waarden['gentpoort']=array('Werk_economie');
+		$waarden['gentpoort']['Werk_economie'] = array
+		(// deze waarden zijn voor de snelheid om de proof of concept te maken er vast in geprogrammeerd
+		'Werk_economie'="84%",
+		'horeca' =>"5",
+		'bank' => "4",
+		'supermarkt' =>"4",
+		'industrie' =>"3",
+		'religie' =>"5"
+		);	
+	}
+}	
+	
+?>
