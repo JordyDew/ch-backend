f0742b41e39b2e6108eb3ad3e611634d7cbf59ca
ianrau9 <albireo9876@gmail.com>
2018-11-09 17:49:05 +0100

    Add files via upload
    sommige syntaxfouten opgelost

--- analyse_werkeneconomie.php
+++ analyse_werkeneconomie.php
@@ -1,31 +1,28 @@
-<?
+<?php
 class werkIndex
 {
-	public:
+	
 	//opvragen van de werkIndex array
-	function get()
+	public function get()
 	{
 		return generateWerk();
 	}
 	
-	private:
 	
-	function generateWerk()
+	private function generateWerk()
 	{
 		$waarden = array('gentpoort');
 		$waarden['gentpoort']=array('Werk_economie');
 		$waarden['gentpoort']['Werk_economie'] = array
-		(// deze waarden zijn voor de snelheid om de proof of concept te maken er vast in geprogrammeerd
-		'Werk_economie'="84%",
+		(// deze waarden zijn voor de snelheid om de proof of concept te maken er vast in geprogrammeerd. In de feitelijke bedoeling is het
+		'Werk_economie'=>"84%",
 		'horeca' =>"5",
 		'bank' => "4",
 		'supermarkt' =>"4",
 		'industrie' =>"3",
 		'religie' =>"5"
 		);	
-		
 		return $waarden;
 	}
 }	
-	
-?>
+//php niet gesloten vanwege include in andere file
