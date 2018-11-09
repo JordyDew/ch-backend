cb9422888fdde685e97dbcd01158660a1d5860d4
ianrau9 <albireo9876@gmail.com>
2018-11-09 23:32:50 +0100

    upgedate php

--- analyse_zorgOverheid.php
+++ analyse_zorgOverheid.php
@@ -12,18 +12,16 @@ class zorgIndex
 	//Deze functie berekent de zorg index. De waarden die berekent moet worden 
 	private function generateZorg()
 	{
-		$waarden=
-		'"Zorg_overheid":
-			{
-			"Zorg_overheid":83,
-			"ziekenhuis":3,
-			"apotheek":5,
-			"huisarts":5,
-			"stadhuis":3,
-			"school":5,
-			"faciliteiten":4
-			}
-		';
+		$waarden= ["Zorg_overheid" => [
+			"Zorg_overheid" => 83,
+			"ziekenhuis" => 3,
+			"apotheek" => 5,
+			"huisarts" => 5,
+			"stadhuis" => 3,
+			"school" => 5,
+			"faciliteiten" => 4
+		]
+		];
 		return $waarden;
 	}
 }	//php niet gesloten vanwege include in andere file
