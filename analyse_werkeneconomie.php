86e46a808369578c10eaeb4666275900b3e14348
ianrau9 <albireo9876@gmail.com>
2018-11-09 19:08:16 +0100

    Add files via upload
    files aangepast om de gewenste json hierarchie te maken

--- analyse_werkeneconomie.php
+++ analyse_werkeneconomie.php
@@ -11,17 +11,18 @@ class werkIndex
 	
 	private function generateWerk()
 	{
-		$waarden = array('gentpoort');
-		$waarden['gentpoort']=array('Werk_economie');
-		$waarden['gentpoort']['Werk_economie'] = array
-		(// deze waarden zijn voor de snelheid om de proof of concept te maken er vast in geprogrammeerd. In de feitelijke bedoeling is het
-		'Werk_economie'=>"84%",
-		'horeca' =>"5",
-		'bank' => "4",
-		'supermarkt' =>"4",
-		'industrie' =>"3",
-		'religie' =>"5"
-		);	
+		// deze waarden zijn voor de snelheid om de proof of concept te maken er vast in geprogrammeerd. In de feitelijke bedoeling is het
+		$waarden = 
+		"{'Werk en economie':
+			{
+			'Werk_economie':'84%',
+			'horeca':'5',
+			'bank' : '4',
+			'supermarkt' :'4',
+			'industrie' :'3',
+			'religie' :'5'
+			}
+		}"	
 		return $waarden;
 	}
 }	
--- analyse_zorgOverheid.php
+++ analyse_zorgOverheid.php
@@ -5,25 +5,25 @@ class zorgIndex
 	//opvragen van de zorgIndex array
 	public function get()
 	{
-		return $this->generateZorg();
+		return generateWerk();
 	}
 	
 	
 	//Deze functie berekent de zorg index
 	private function generateZorg()
 	{
-		$waarden=$waarden = array('gentpoort');
-		$waarden['gentpoort']=array('Zorg_overheid');
-		$waarden['gentpoort']['Zorg_overheid'] = array
-		(
-			'Zorg_overheid'=>"83%",
-			'ziekenhuis'=>"3",
-			'apotheek'=>"5",
-			'huisarts'=>"5",
-			'stadhuis'=>"3",
-			'school'=>"5",
-			'faciliteiten' =>"4"
-		);
+		$waarden=
+		"{'Zorg_overheid':
+			{
+			'Zorg_overheid':'83%',
+			'ziekenhuis':'3',
+			'apotheek':'5',
+			'huisarts':'5',
+			'stadhuis':'3',
+			'school':'5',
+			'faciliteiten' :'4'
+			}
+		}"
 		return $waarden;
 	}
 }	//php niet gesloten vanwege include in andere file
