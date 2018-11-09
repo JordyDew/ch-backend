a5a0b34bdefe60959f46ceba9ed5a6dde6f84288
jordydeweer <jordyydeweer@gmail.com>
2018-11-09 22:16:25 +0100

    o

--- mobiliteit.php
+++ mobiliteit.php
@@ -6,13 +6,13 @@ class mobiliteit
     public function get()
     {
         // The values of the environment parameters (hard-coded)
-        $waarden = "{'mobiliteit':{
-            'mobiliteit': '76%', 
-            'bus': 5,
-            'trein': 4,
-            'snelheidslimiet': 3,
-            'afstand_tot_grootsteden': 3,
-            'laadpalen': 3}}";
+        $waarden = '{"mobiliteit":{
+            "mobiliteit": 76, 
+            "bus": 5,
+            "trein": 4,
+            "snelheidslimiet": 3,
+            "afstand_tot_grootsteden": 3,
+            "laadpalen": 3}}';
 
         return $waarden;
     }
