62613659a92eb281169faef41e906c051d58fb72
jordydeweer <jordyydeweer@gmail.com>
2018-11-10 01:32:43 +0100

    Add location to JSON

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php
-require_once('analyse_werkeneconomie.php');
+require_once ('analyse_werkeneconomie.php');
 $werk = new werkIndex();
-require_once('natuur.php');
+require_once ('natuur.php');
 $milieu = new natuur();
-require_once('mobiliteit.php');
+require_once ('mobiliteit.php');
 $mobi = new mobiliteit();
-require_once('analyse_zorgOverheid.php');
+require_once ('analyse_zorgOverheid.php');
 $zorg = new zorgIndex();
 
 $data = [
-    'Zorg_overheid' => $zorg->get(),
-    'mobiliteit' => $mobi->get(),
-    'Werk_en_economie' => $mobi->get(),
-    'milieu' => $milieu->get()
+    'gentpoort' => [
+        'Zorg_overheid' => $zorg->get(),
+        'mobiliteit' => $mobi->get(),
+        'Werk_en_economie' => $mobi->get(),
+        'milieu' => $milieu->get()
+    ]
 ];
 echo json_encode($data);
 ?>
