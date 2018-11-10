dd41b5cb4f8cd9b09a18861a5588a6e9452fed30
jordydeweer <jordyydeweer@gmail.com>
2018-11-10 01:28:27 +0100

    Create arrays of the raw JSON data; to solve problems

--- analyse_zorgOverheid.php
+++ analyse_zorgOverheid.php
@@ -1,27 +1,26 @@
 <?php
+
 class zorgIndex
 {
-	
-	//opvragen van de zorgIndex array
-	public function get()
-	{
-		return $this->generateZorg();
-	}
-	
-	
-	//Deze functie berekent de zorg index. De waarden die berekent moet worden 
-	private function generateZorg()
-	{
-		$waarden= ["Zorg_overheid" => [
-			"Zorg_overheid" => 83,
-			"ziekenhuis" => 3,
-			"apotheek" => 5,
-			"huisarts" => 5,
-			"stadhuis" => 3,
-			"school" => 5,
-			"faciliteiten" => 4
-		]
-		];
-		return $waarden;
-	}
+
+    // opvragen van de zorgIndex array
+    public function get()
+    {
+        return $this->generateZorg();
+    }
+
+    // Deze functie berekent de zorg index. De waarden die berekent moet worden
+    private function generateZorg()
+    {
+        $waarden = [
+            "Zorg_overheid" => 83,
+            "ziekenhuis" => 3,
+            "apotheek" => 5,
+            "huisarts" => 5,
+            "stadhuis" => 3,
+            "school" => 5,
+            "faciliteiten" => 4
+        ];
+        return $waarden;
+    }
 }	//php niet gesloten vanwege include in andere file
--- index.php
+++ index.php
@@ -8,7 +8,11 @@ $mobi = new mobiliteit();
 require_once('analyse_zorgOverheid.php');
 $zorg = new zorgIndex();
 
-$data = "{".$zorg->get().",".$mobi->get().",".$werk->get().",".$milieu->get()."}";
-
-echo json_encode($zorg->get());
+$data = [
+    'Zorg_overheid' => $zorg->get(),
+    'mobiliteit' => $mobi->get(),
+    'Werk_en_economie' => $mobi->get(),
+    'milieu' => $milieu->get()
+];
+echo json_encode($data);
 ?>
